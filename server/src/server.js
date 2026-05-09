423f754d283203f7eb9429b717057e67b77ec467
abd <abdalhafez.altamimi48@gmail.com>
2026-05-09 13:44:42 +0300

    connect mongodb database

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
@@ -8,6 +16,8 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log("server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
 });
