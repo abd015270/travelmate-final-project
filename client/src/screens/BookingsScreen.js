1ce486d38795886538aa1da1b11681483edfc224
abd <abdalhafez.altamimi48@gmail.com>
2026-05-25 15:48:32 +0300

    recreate expo client with blank javascript template

--- client/src/screens/BookingsScreen.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function BookingsScreen() {
-  return (
-    <View>
-      <Text>Bookings Screen</Text>
-    </View>
-  );
-}
--- client/src/screens/TripsScreen.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function TripsScreen() {
-  return (
-    <View>
-      <Text>Trips Screen</Text>
-    </View>
-  );
-}
